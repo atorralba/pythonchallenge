ec8385b231f33438918e5e975f8c6ac629230513
the3rdman <thirdman91@gmail.com>
2016-05-30 17:20:28 +0200

    Solved good

--- good/solve.py
+++ good/solve.py
@@ -3,6 +3,7 @@
 import urllib2
 import base64
 import re
+from PIL import Image, ImageDraw
 
 request = urllib2.Request("http://www.pythonchallenge.com/pc/return/good.html")
 base64string = base64.encodestring('%s:%s' % ('huge', 'file')).replace('\n', '')
@@ -11,4 +12,18 @@ result = urllib2.urlopen(request).read()
 
 first = [ int(x) for x in re.search(r'first:\n([0-9],?\n?)*', result).group().replace('first:', '').replace('\n', '').strip().split(',') ]
 second = [ int(x) for x in re.search(r'second:\n([0-9],?\n?)*', result).group().replace('second:', '').replace('\n', '').strip().split(',') ]
-print first + second
+maximum = max(max(first),max(second))
+
+image = Image.new('RGB', (maximum, maximum))
+draw = ImageDraw.Draw(image)
+for i in range(0, len(first)-3, 2):
+	first_px = (first[i], first[i+1])
+	second_px = (first[i+2], first[i+3])
+	draw.line((first_px,second_px), fill=255)
+
+for i in range(0, len(second)-3, 2):
+	first_px = (second[i], second[i+1])
+	second_px = (second[i+2], second[i+3])
+	draw.line((first_px,second_px), fill=255)
+
+image.show()
